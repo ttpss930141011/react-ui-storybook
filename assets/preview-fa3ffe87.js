31af4849113438e5e027386fc8b26d2480224e0f
Justin Xiao <o927416847@gmail.com>
2023-05-10 16:40:29 +0800

    Updates

---
+++ assets/preview-fa3ffe87.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-4d1c0d90.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-ee5b63d8.js"),["./DocsRenderer-EYKKDMVH-ee5b63d8.js","./iframe-4d1c0d90.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-5792738b.js","./index-9900c05c.js","./index-429c3002.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-fa3ffe87.js.map
